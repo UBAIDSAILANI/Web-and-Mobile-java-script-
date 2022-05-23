70f0e836eedabb7206b362203b77ce2464ac56f7
Ubaid ur Rehman <77199751+UBAIDSAILANI@users.noreply.github.com>
2022-05-23 20:15:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// var gender = prompt("Enter your gender");
+// if (gender === "male"){
+//     alert("Hello Sir")}
+
+// else if (gender === "female"){
+//     alert("Hello Mam")
+// }
+// var abcCompany = prompt("Hiring a developer")
+var html = prompt("Do you know Html");
+var css = prompt("Do you know Css");
+if (html || css === "yes")
+{    alert("Good Luck");}
+if (html && css === "no")
+    {alert("Good bye")
+}
+var js = prompt("Do you know Javascript")
+var city = prompt("Enter the city name")
+var experience = prompt("Enter your experience");
+var age = prompt("Enter your age");
+
+if (js === "yes"  && age >= 18 && experience >= 2 && city === "karachi")
+
+    alert("Your are Selected");
+else {
+    alert("try next time");
+}
+
+// if(javascript === "yes");
+
+// if(city === "karachi");
+
+// if (experience >= "2years");
